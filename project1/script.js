3d27beedd1416b0185c37410cde552e81ed70d5f
kolkvicija <vaka.vice@gmail.com>
2024-03-15 19:38:00 +0100

    JS added, working on responsiveness

---
+++ project1/script.js
@@ -0,0 +1,27 @@
+
+window.addEventListener('scroll', function() {
+    const scrolled = window.scrollY > 0;
+    document.body.classList.toggle('scrolled', scrolled);
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+var boxes = document.querySelectorAll('.auone');
+
+function checkScroll() {
+var triggerBottom = window.innerHeight * 0.8;
+
+boxes.forEach(box => {
+  var boxTop = box.getBoundingClientRect().top;
+
+  if (boxTop < triggerBottom) {
+    box.classList.add('show');
+  } else {
+    box.classList.remove('show');
+  }
+});
+}
+
+checkScroll();
+
+window.addEventListener('scroll', checkScroll);
+});
